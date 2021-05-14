78a04084ee4859c90b9b6573a2452aa0f512aa93
jisungbin <ji@sungb.in>
2021-05-15 06:34:43 +0900

    :dagger:

--- python/ChatRoomGui.py
+++ python/ChatRoomGui.py
@@ -1,27 +1,42 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import json
+import pyrebase
 
-messages = []
+config = {
+    'apiKey': 'AIzaSyCJCjo-Gwpy_yFBu18x3Ng1tt0_ElLyi30',
+    "authDomain": 'pythonkakaopcwrapper.firebaseapp.com',
+    'databaseURL': 'https://pythonkakaopcwrapper-default-rtdb.firebaseio.com',
+    'storageBucket': 'pythonkakaopcwrapper.appspot.com'
+}
+
+firebase = pyrebase.initialize_app(config)
 
 
 class ChatRoomGui(QWidget):
-    def __init__(self, _room):
+    def __init__(self, _room, _messages, device_number):
+        self.db = firebase.database().child(device_number).child('send')
         self.room = _room
+        self.messages = _messages
         super().__init__()
 
         self.listview = QListView(self)
         self.edittext = QTextEdit(self)
-        self.button = QPushButton("메시지 전송", self)
+        self.button = QPushButton('메시지 전송', self)
         self.init()
 
     def init(self):
         self.setFixedSize(500, 500)
         self.setWindowTitle(f'{self.room} 채팅방')
 
+        model = QStandardItemModel()
+        for message in self.messages:
+            model.appendRow(QStandardItem(f'[{message.get("sender")}] - {message.get("message")}'))
+        self.listview.setModel(model)
         self.listview.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.listview.resize(500, 400)
 
-        self.edittext.setPlaceholderText("메시지 입력...")
+        self.edittext.setPlaceholderText('메시지 입력...')
         self.edittext.setAcceptRichText(False)
         self.edittext.resize(300, 50)
         self.edittext.move(30, 425)
@@ -33,15 +48,19 @@ class ChatRoomGui(QWidget):
 
     def message_send(self):
         message = self.edittext.toPlainText()
+        json_string = "{" + f'"sender": "나", "message": "{message}"' + "}"
+        json_value = json.loads(json_string)
         if len(message) != 0:
-            messages.append(message)
+            self.messages.append(json_value)
             model = QStandardItemModel()
-            for message in messages:
-                model.appendRow(QStandardItem(message))
+            for message in self.messages:
+                model.appendRow(QStandardItem(f'[{message.get("sender")}] - {message.get("message")}'))
             self.listview.setModel(model)
             self.edittext.clear()
+
+            self.db.push(json_string)
         else:
             dialog = QMessageBox(self)
-            dialog.setWindowTitle("경고")
-            dialog.setText("메시지를 입력하세요.")
+            dialog.setWindowTitle('경고')
+            dialog.setText('메시지를 입력하세요.')
             dialog.show()
--- python/index.py
+++ python/index.py
@@ -1,21 +1,62 @@
 import json
 import sys
-from PyQt5.QtWidgets import QApplication
 from ChatRoomGui import ChatRoomGui
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import pyrebase
-
+import turtle
+from plyer import notification
 
 rooms = []
 messages = {}
 
+device_number = input('카카오톡이 설치된 기기 번호 입력: ')
+
+
+def open_chat_room(room):
+    global messages
+    notification.notify(
+        title='알림',
+        message='현재 창을 닫으면 카톡방이 열립니다.',
+        timeout=5,
+    )
+    app = QApplication(sys.argv)
+    _ = ChatRoomGui(room, messages[room], device_number)
+    sys.exit(app.exec_())
+
+
+def window_click(x, y):
+    print(y)
+    if 200 <= y <= 250:
+        open_chat_room(rooms[0])
+    elif 150 <= y <= 199:
+        open_chat_room(rooms[1])
+    elif 100 <= y <= 149:
+        open_chat_room(rooms[2])
+
+
+def draw_rooms():
+    for i in range(0, len(rooms)):
+        room = rooms[i]
+        y = 200 - (50 * i)
+        print(y)
+        turtle.penup()
+        turtle.goto(0, y)
+        turtle.pendown()
+        turtle.write(room, font=('나눔고딕', 20))
+
 
 def stream_handler(message):
-    room = message["path"].split('/')[1]
-    data = message["data"]
+    global messages
+    global rooms
+    room = message['path'].split('/')[1]
+    data = message['data']
     if room == '' or data == 'None':
         return
     if room not in rooms:
         rooms.append(room)
+        draw_rooms()
+        print(rooms)
     json_value = json.loads(data)
     if room not in messages:
         messages[room] = [json_value]
@@ -23,26 +64,25 @@ def stream_handler(message):
         new_messages = messages[room]
         new_messages.append(json_value)
         messages[room] = new_messages
-    print(messages[room])
 
 
 config = {
-    "apiKey": "AIzaSyCJCjo-Gwpy_yFBu18x3Ng1tt0_ElLyi30",
-    "authDomain": "pythonkakaopcwrapper.firebaseapp.com",
-    "databaseURL": "https://pythonkakaopcwrapper-default-rtdb.firebaseio.com",
-    "storageBucket": "pythonkakaopcwrapper.appspot.com"
+    'apiKey': 'AIzaSyCJCjo-Gwpy_yFBu18x3Ng1tt0_ElLyi30',
+    "authDomain": 'pythonkakaopcwrapper.firebaseapp.com',
+    'databaseURL': 'https://pythonkakaopcwrapper-default-rtdb.firebaseio.com',
+    'storageBucket': 'pythonkakaopcwrapper.appspot.com'
 }
 
 firebase = pyrebase.initialize_app(config)
 
-device_number = input('카카오톡이 설치된 기기 번호 입력: ')
-
 db = firebase.database().child(device_number)
-db.child("status").push("connected")
-db.child(device_number).child("receive").stream(stream_handler)
+db.child('status').push('connected')
+db.child(device_number).child('receive').stream(stream_handler)
 
+window = turtle.Screen()
+window.title('카카오톡 PC버전 따라하기')
+window.setup(500, 500)
+window.onclick(window_click)
 
-def open_chat_room():
-    app = QApplication(sys.argv)
-    ChatRoomGui("aaa")
-    sys.exit(app.exec_())
+turtle.speed(10)
+turtle.mainloop()
